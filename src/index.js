c0ef019cae2c4954dd8305d9cf17c4f901a2c439
okj203 <okj203@gmail.com>
2020-08-19 23:05:19 -0400

    modal

--- src/index.js
+++ src/index.js
@@ -175,5 +175,23 @@ function handleResizeClick(e) {
   }
 }
 
+// modal
+var modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    var modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    var exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
+
 initSetting();
 initEvent();
